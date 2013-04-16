26edd2b4aba6d9e7c30d5f2d120982d0df2f0e53
Jon Patrick Lim <cirpat007@gmail.com>
2013-04-16 15:27:48 +0800

    added a folder, changed ReadMe. yeah. first commit

---
+++ ojtproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ojtproject::Application.config.secret_token = '44e1ed237cca4227eb02a398c5d2461f5e76425a07ad5700007cfd9beaa0f6a31d7f55d4ee7850b6c4a7147aa7a7696fbffd10c4200ec9158501ecc1ff899fc1'
