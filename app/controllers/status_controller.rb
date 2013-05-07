f7e9a10cd2c8f5c5918998ca5ccc37f42f8a72e6
enriquebatoon <enriquebatoon@gmail.com>
2013-05-07 18:32:00 +0800

    change date in announcement and twitter and position of view commentst

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,11 +4,11 @@ require 'uri'
 class StatusController < ApplicationController
   def index
 
-  	tweet = Feedzirra::Feed.fetch_and_parse("http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=offerchat")
-  	@tweets = tweet.entries.first(5)
+  	#tweet = Feedzirra::Feed.fetch_and_parse("http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=offerchat")
+  	#@tweets = tweet.entries.first(5)
 
   	@site_is_up = pingSite
-    @server_is_up = pingServer
+    @server_is_up = pingServer 
 
 
     
@@ -33,8 +33,8 @@ class StatusController < ApplicationController
   def view
   	@post = Announcement.find(params[:id])
 
-  	tweet = Feedzirra::Feed.fetch_and_parse("http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=offerchat")
-  	@tweets = tweet.entries.first(5)
+  	#tweet = Feedzirra::Feed.fetch_and_parse("http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=offerchat")
+  	#@tweets = tweet.entries.first(5)
   end
 
   def announce(title,message)
@@ -42,14 +42,14 @@ class StatusController < ApplicationController
   	
 	if first
 		if first.body != message 
-		  		post = Announcement.new(:title => "#{title}", :body => "#{message}" ,:auto=>true )
+		  		post = Announcement.new(:title => "#{title}", :body => "#{message}" ,:auto=>true, :created_at=>Time.now.strftime("%B %d, %Y %I:%M %p") )
 		  		post.save
-		  		Twitter.update("[Update:" + Date.today.strftime("%B %d") + Time.now.strftime("-%I:%M")+"] #{message}" )
+		  		#Twitter.update("[Update:" + Date.today.strftime("%B %d") + Time.now.strftime("-%I:%M %p")+"] #{message}" )
 		end
 	else
 		post = Announcement.new(:title => "#{title}", :body => "#{message}", :auto=>true)
 	  	post.save
-	  	Twitter.update("[Update:" + Date.today.strftime("%B %d") + Time.now.strftime("-%I:%M")+"] #{message}" )
+	  	#Twitter.update("[Update:" + Date.today.strftime("%B %d") + Time.now.strftime("-%I:%M %p")+"] #{message}" )
 	end
 
   end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,5 @@
 class Announcement < ActiveRecord::Base
-	attr_accessible :title, :body, :auto
+	attr_accessible :title, :body, :auto, :created_at
 	scope :automatic, where(:auto => true)
 
 	validates :title,  :presence => true
